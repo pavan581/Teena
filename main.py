1a37d7352a6a8c6bebd80256879079b8fac8e5e7
pavan <sairam.g581@gmail.com>
2020-07-07 21:24:10 +0530

    Changed to ask for username and added feature to take new entry.

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-#!/usr/bin/env python3
-
 import os
-import time
 import datetime
 
+import data
 import voice
 import wishes
 import features
 
 
-contacts ={
-
-    "me"            :"+919573486634",
-    "hari"          :"+919885112557",
-    "raja"          :"+918106061578",
-    "akka"          :"+919493605566",
-    "srinu"         :"+916304571505"
-    
-    }
-
 
 if __name__ == "__main__":
-    voice._out(wishes.wishMe())
+    try:
+        username = data._read("username")
+        voice._out(wishes.wishMe(username))
+        
+    except:
+        data.__init__()
+        while True:
+            voice._out(wishes.wishMe())
+            username = voice._in("Your name___")
+            if username != 'none':
+                data._write("username",username)
+                break
     
     while True:
         print(".-- . .-.. -.-. --- -- .   -... .- -.-. -.-")
@@ -30,21 +29,19 @@ if __name__ == "__main__":
         if 'help' in MyText:
             voice._out(features.help())
 
-        elif 'what\'s the time' == MyText:
+        elif 'new entry' in MyText:
+            voice._out("Enter with keyboard.")
+            data._write(input("Entry name___"),input("Entry value___"))
+
+        elif 'time' in MyText:
             voice._out(f"{int(datetime.datetime.now().hour)}:{int(datetime.datetime.now().minute)}")
             
         elif 'how are you' in MyText:
             voice._out(wishes.hru())
 
-        elif 'good night' in MyText:
+        elif 'good night' in MyText or 'exit' in MyText:
             voice._out(wishes.gn())
             exit()
-
-        elif 'translate' in MyText:
-            MyText = MyText.replace("translate","")
-            fromlang = voice._in("from..")
-            tolang = voice._in("to..")
-            voice._out(features.translator(fromlang,tolang,MyText))
 
         elif 'wikipedia' in MyText:
             voice._out(features.wiki(MyText))
@@ -52,22 +49,28 @@ if __name__ == "__main__":
         elif 'music' in MyText:
             features.spotify(MyText)
 
+        elif 'search for' in MyText or 'in google' in MyText:
+            features.google(MyText)
+                
+        elif 'translate' in MyText:
+            MyText = MyText.replace("translate","")
+            fromlang = voice._in("from..")
+            tolang = voice._in("to..")
+            voice._out(features.translator(fromlang,tolang,MyText))
+
         elif 'whatsapp' in MyText:
             while True:
                 phonetext = voice._in("to..")
                 if phonetext != 'none':
-                    phonenum = contacts.get(phonetext)
+                    phonenum = data._read(phonetext)
                     if phonenum == None:
                         print("Sorry! Can't get the number.")
                         break
                     else:
                         while True:
-                            msg = voice._in("sat the message..")
+                            msg = voice._in("say the message..")
                             if msg != None and msg != 'none':
                                 features.whatsapp(phonenum,msg)
                                 break
                     break
-                    
-        elif 'search for' in MyText or 'in google' in MyText:
-            features.google(MyText)
             
